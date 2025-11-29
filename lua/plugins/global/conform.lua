5d22a0aba9e4588013a562c4694a5f12cf145b3d
felipeC20312 <felipecaetanodev@gmail.com>
2025-11-28 23:42:02 -0300

    Update - 281125.1
    Fix on Confform.lua plugin.

--- lua/plugins/global/conform.lua
+++ lua/plugins/global/conform.lua
@@ -2,19 +2,13 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      javascript = { "prettierd ", "prettier" },
-      javascriptreact = { "prettierd ", "prettier" },
-      typescript = { "prettierd ", "prettier" },
-      typescriptreact = { "prettierd ", "prettier" },
+      javascript = { "prettierd", "prettier" },
+      javascriptreact = { "prettierd", "prettier" },
+      typescript = { "prettierd", "prettier" },
+      typescriptreact = { "prettierd", "prettier" },
       css = { "prettierd", "prettier" },
       html = { "prettierd", "prettier" },
       scss = { "prettierd", "prettier" },
-    },
-    formatters = {
-      prettier = {
-        command = vim.fn.stdpath("data") .. "/mason/bin/prettierd",
-        args = { "--stdin-filepath", "$FILENAME" },
-      },
     },
   },
 }
