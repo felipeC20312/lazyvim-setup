be8aab9c69561f4aa621c2b6654078053a4fb313
felipeC20312 <felipecaetanodev@gmail.com>
2026-01-23 11:28:17 -0300

    update - 230126.1

--- lua/plugins/global/mini-icons.lua
+++ lua/plugins/global/mini-icons.lua
@@ -3,61 +3,49 @@ return {
   lazy = false,
   opts = {
     extension = {
-      md = { glyph = "", hl = "MiniIconsMd" },
-      lua = { glyph = "󰢱", hl = "MiniIconsLua" },
-      json = { glyph = "󰘦", hl = "MiniIconsJson" },
-      git = { glyph = "󰊢", hl = "MiniIconsGit" },
-      kt = { glyph = "", hl = "MiniIconsKotlin" },
-      kts = { glyph = "", hl = "MiniIconsKotlin" },
+      md = { glyph = "", hl = "dkyCian" },
+      lua = { glyph = "󰢱", hl = "dkyBlue" },
+      git = { glyph = "󰊢", hl = "dkyOrangeDark" },
+      json = { glyph = "󰘦", hl = "dkyYellow" },
+      kt = { glyph = "", hl = "dkyPurpleDeep" },
+      kts = { glyph = "", hl = "dkyPurple" },
     },
     file = {
-      [".go-version"] = { glyph = "", hl = "MiniIconsBlue" },
-      [".gitignore"] = { glyph = "󰊢", hl = "MiniIconsGit" },
-      [".gitattributes"] = { glyph = "󰊢", hl = "MiniIconsGit" },
-      ["package.json"] = { glyph = "󰎙", hl = "MiniIconsJson" },
-      ["tsconfig.json"] = { glyph = "󰘦", hl = "MiniIconsJson" },
-      ["gradlew"] = { glyph = "", hl = "MiniIconsGradle" },
-      ["gradlew.bat"] = { glyph = "", hl = "MiniIconsGradleBat" },
+      [".gitignore"] = { glyph = "󰊢", hl = "dkyOrange" },
+      ["gradlew"] = { glyph = "", hl = "dkyCian" },
+      [".go-version"] = { glyph = "", hl = "dkyBlue" },
+      ["package.json"] = { glyph = "󰎙", hl = "dkyGreen" },
+      [".gitattributes"] = { glyph = "󰊢", hl = "dkyOrange" },
+      ["tsconfig.json"] = { glyph = "󰘦", hl = "dkyYellow" },
+      ["gradlew.bat"] = { glyph = "", hl = "dkyBlue" },
     },
     filetype = {
-      gotmpl = { glyph = "󰟓", hl = "MiniIconsGrey" },
+      gotmpl = { glyph = "󰟓", hl = "dkyCian" },
     },
     directory = {
-      app = { glyph = "󰉋", hl = "MiniIconsApp" },
-      application = { glyph = "󰉋", hl = "MiniIconsApp" },
-      domain = { glyph = "󱋣", hl = "MiniIconsDomain" },
-      config = { glyph = "󱁿", hl = "MiniIconsConfig" },
-      persistency = { glyph = "󰉋", hl = "MiniIconsPersistency" },
-
-      src = { glyph = "󰉋", hl = "MiniIconsSrc" },
-      test = { glyph = "󰙨", hl = "MiniIconsTest" },
+      app = { glyph = "󰉋", hl = "dkyRed" },
+      src = { glyph = "󰉋", hl = "dkyOrangeDark" },
+      test = { glyph = "󰙨", hl = "dkyYellow" },
+      domain = { glyph = "󱋣", hl = "dkyOrange" },
+      config = { glyph = "󱁿", hl = "dkyCian" },
+      application = { glyph = "󰉋", hl = "dkyRed" },
+      persistency = { glyph = "󰉋", hl = "dkyPurple" },
     },
     default = {
-      directory = { glyph = "󰉋", hl = "MiniIconsFolder" },
+      directory = { glyph = "󰉋", hl = "dkyGray" },
     },
   },
   init = function()
-    -- Arquivos
-    vim.api.nvim_set_hl(0, "MiniIconsMd", { fg = "#94e2d5", bold = true }) -- teal
-    vim.api.nvim_set_hl(0, "MiniIconsLua", { fg = "#89bafa", bold = true }) -- blue
-    vim.api.nvim_set_hl(0, "MiniIconsJson", { fg = "#f9e2af", bold = true }) -- yellow
-    vim.api.nvim_set_hl(0, "MiniIconsGit", { fg = "#f38b8b", bold = true }) -- red
-    vim.api.nvim_set_hl(0, "MiniIconsKotlin", { fg = "#94e2d5", bold = true })
-    vim.api.nvim_set_hl(0, "MiniIconsGradle", { fg = "#94e2d5", bold = true })
-    vim.api.nvim_set_hl(0, "MiniIconsGradleBat", { fg = "#f9e2af", bold = true })
-
-    -- Diretórios principais
-    vim.api.nvim_set_hl(0, "MiniIconsApp", { fg = "#f9e2af", bold = true })
-    vim.api.nvim_set_hl(0, "MiniIconsDomain", { fg = "#f38e7d", bold = true }) -- magenta
-    vim.api.nvim_set_hl(0, "MiniIconsConfig", { fg = "#f38b8b", bold = true })
-    vim.api.nvim_set_hl(0, "MiniIconsPersistency", { fg = "#94e2d5", bold = true })
-
-    -- Outros diretórios
-    vim.api.nvim_set_hl(0, "MiniIconsSrc", { fg = "#fab387", bold = true })
-    vim.api.nvim_set_hl(0, "MiniIconsTest", { fg = "#f9e2af", bold = true })
-
-    -- Defaults
-    vim.api.nvim_set_hl(0, "MiniIconsFolder", { fg = "#6c7086", bold = true })
+    vim.api.nvim_set_hl(0, "dkyRed", { fg = "#f38b8b", bold = true })
+    vim.api.nvim_set_hl(0, "dkyGray", { fg = "#6c7086", bold = true })
+    vim.api.nvim_set_hl(0, "dkyCian", { fg = "#94e2d5", bold = true })
+    vim.api.nvim_set_hl(0, "dkyGreen", { fg = "#BFDA89", bold = true })
+    vim.api.nvim_set_hl(0, "dkyBlue", { fg = "#89bafa", bold = true })
+    vim.api.nvim_set_hl(0, "dkyYellow", { fg = "#f9e2af", bold = true })
+    vim.api.nvim_set_hl(0, "dkyOrange", { fg = "#fab387", bold = true })
+    vim.api.nvim_set_hl(0, "dkyPurple", { fg = "#B7A7FF", bold = true })
+    vim.api.nvim_set_hl(0, "dkyPurpleDeep", { fg = "#8C7CFF", bold = true })
+    vim.api.nvim_set_hl(0, "dkyOrangeDark", { fg = "#f38e7d", bold = true })
   end,
   config = function(_, opts)
     require("mini.icons").setup(opts)
