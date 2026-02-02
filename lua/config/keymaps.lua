428bd5a904a6535c52f5d9374816dfe154f0f2fc
felipeC20312 <felipecaetanodev@gmail.com>
2026-02-02 14:40:24 -0300

    update - 020226.1

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,25 @@ vim.keymap.set("n", "<leader>ctw", function()
   vim.wo.wrap = not vim.wo.wrap
 end, { desc = "Toggle wrap" })
 
+-- SortJSON jq
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "json",
+  callback = function(ev)
+    vim.keymap.set("n", "<leader>cj", function()
+      if vim.fn.executable("jq") == 1 then
+        vim.cmd("silent %!jq --sort-keys .")
+      elseif vim.fn.executable("python3") == 1 then
+        vim.cmd("silent %!python3 -m json.tool --sort-keys")
+      else
+        vim.notify("Install jq or python3 for JSON sorting", vim.log.levels.WARN)
+      end
+    end, {
+      buffer = ev.buf,
+      desc = "󰘦 Sort JSON",
+    })
+  end,
+})
+
 -- Flutter keymaps
 -- local flutter_maps = {
 --   { "<leader>fr", "<cmd>FlutterRun<cr>", desc = "Flutter Run" },
